b89c9028b12ad06d0344bc8211cb9cd6b84c9169
Siddharth <siddharthsampath98@gmail.com>
2020-05-06 20:06:30 +0530

    Problem 6

---
+++ problem6.py
@@ -0,0 +1,25 @@
+'''
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+Another hash table problem...
+'''
+# Time O(n) Space O(n)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        frequency = {}
+        n = len(nums)
+        for ele in nums:
+            frequency[ele] = frequency.get(ele,0) + 1
+            if frequency[ele] > n//2:
+                return ele
